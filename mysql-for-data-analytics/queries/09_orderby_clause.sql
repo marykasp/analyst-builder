0890e219f5c6fe584519bdf5d72cf3b3eae44aa8
marykasp <kasparian.mary@gmail.com>
2026-06-16 12:21:57 -0500

    add order by clause to sort columns in ascending or descending order

---
+++ mysql-for-data-analytics/queries/09_orderby_clause.sql
@@ -0,0 +1,22 @@
+-- =============================================
+-- Lesson: ORDER BY clause
+-- Course: MySQL for Data Analytics
+-- Date: June 16, 2026
+-- =============================================alter
+
+USE bakery;
+
+-- ORDER by first name in ascending order
+SELECT *
+FROM customers
+ORDER BY first_name;
+
+-- ORDER first by state ascending then by total money spent
+SELECT *
+FROM customers
+ORDER BY state, total_money_spent DESC;
+
+-- ORDER columns by position of columns- 8th column which is the state
+SELECT *
+FROM customers
+ORDER BY 8 DESC, 9 ASC;
