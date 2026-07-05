9267bae0ac81e9864faed7110da59dcf07e0f428
marykasp <kasparian.mary@gmail.com>
2026-07-05 10:49:09 -0500

    add cast and convert query functions to convert one data type to another

---
+++ mysql-for-data-analytics/queries/17_cast_convert_functions.sql
@@ -0,0 +1,17 @@
+-- =============================================
+-- Lesson: CAST and CONVERt Functions
+-- Course: MySQL for Data Analytics
+-- Date: July 5th, 2026
+-- =============================================
+
+# Both CAST and CONVERT functions are used to convert a value from one data type to another. However, there is a slight difference in their syntax and usage.
+
+# CAST(expression as type)
+# CONVERT(expression, type)
+
+USE bakery;
+
+SELECT birth_date,
+CAST(birth_date AS DATETIME),
+CONVERT(birth_date, DATETIME)
+FROM customers;
